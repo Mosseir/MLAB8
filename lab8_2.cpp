44a7623e529b5c2e180b4f548905233a97f36128
Mosseir <glock18_123@hotmail.com>
2022-12-22 00:26:31 +0700

    Done

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,34 +1,43 @@
-//Dialog of the first example is given below.
-
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
 #include <iostream>
 #include <string>
 using namespace std;
 
-string name(string x , string y){
-}
-
 int main(){
-    string a,b,c,d,e,f;
-    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n" << "?????: ";
-    cin >> a;
+    string name , mov , day ,smth;
+    int id;
     
-    cout << "Fahsai: Wow!!! " << name(a,b) << " is a really cool name." ;
+//input name  include space
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n" << "?????: "; 
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl ;
+    
+//input id and calculate gear of entaneer
+    
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+    cout << name << ": ";
+    cin >> id;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR " << (id/10000000)-12 <<". I have a free movie ticket for you." << endl;
+
+//movie name
+
+    cout << "Fahsai: Let's go to the cinema together!!!" << "\n" << "Fahsai: What movie do you want to watch?" << endl ;
+    cout << name <<": ";
+    getline(cin,mov);
+
+//time to see movie
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+    cout << name <<": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day <<"....that is OK!!! I'm looking forward to watching " << mov << " with you." << endl;
+//say something hahaha
+    cout << name <<": ";
+    getline(cin,smth);
+
+//end of situation
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
 
     return 0;
-}
+//All done!
+}	
